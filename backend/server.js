ce9b593bca581e10ef20a9eae23dde5569b49da6
Sujit <sujitkhopade2001@gmail.com>
2022-01-19 15:12:29 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -35,14 +35,29 @@ app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_I
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+
+// Production Route 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.get('*', (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, '../frontend', 'build', 'index.html'))
+    })
 } else {
     app.get('/', (req, res) => {
-        res.send('Api is running')
+        res.send('API is running...')
     })
 }
+
+
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '/frontend/build')))
+//     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+// } else {
+//     app.get('/', (req, res) => {
+//         res.send('Api is running')
+//     })
+// }
 
 
 app.use(notFound)
